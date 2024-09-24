c3776cf0e3b4b008a9f1ad74fbf1d08dbb710017
MoniLopez <133059943+MoniLopez@users.noreply.github.com>
2024-09-24 17:53:57 -0600

    Se agrega componente para menu y busqueda close #3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { BusquedaComponent } from './busqueda/busqueda.component';
+import { MenuComponent } from './menu/menu.component';
+import { BusquedaModule } from './busqueda/busqueda.module';
+
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full'}, //Redirije a login
-  { path: 'login', component: LoginComponent}, //Indica quién es login
-  { path: 'busqueda', component: BusquedaComponent}, 
+  { path: '', redirectTo: 'login', pathMatch: 'full'}, //Redirije a login
+  { path: 'login', component: LoginComponent }, //Indica quien es login, no tiene routing el componente
+  {
+    path: 'dashboard', component: MenuComponent,
+    children: [
+      {
+        path: 'busqueda',
+        loadChildren: () => import('./busqueda/busqueda.module').then(m => m.BusquedaModule)
+      }
+    ],
+    
+  }
 ];
 
 @NgModule({
